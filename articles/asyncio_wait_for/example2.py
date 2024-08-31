2b4a77e9c0f54e82f80984288763821f7072a08e
bozokopic <bozokopic@users.noreply.github.com>
2024-08-31 19:08:34 +0000

    deploy: 4f8af50bfc676140ba3ec27d93216f6ba3c9b5bd

---
+++ articles/asyncio_wait_for/example2.py
@@ -0,0 +1,53 @@
+import asyncio
+import contextlib
+import itertools
+
+
+async def produce(queue: asyncio.Queue):
+    try:
+        for i in itertools.count(1):
+            queue.put_nowait(i)
+            await asyncio.sleep(1)
+
+    finally:
+        print('closing produce')
+
+
+async def consume(queue: asyncio.Queue):
+    try:
+        while True:
+            try:
+                result = await asyncio.wait_for(queue.get(), timeout=0.5)
+                print(result)
+
+            except asyncio.TimeoutError:
+                print('timeout')
+
+    finally:
+        print('closing consume')
+
+
+async def other_work(delay: float):
+    await asyncio.sleep(delay)
+
+
+async def main():
+    queue = asyncio.Queue()
+
+    producer = asyncio.create_task(produce(queue))
+    consumer = asyncio.create_task(consume(queue))
+
+    await other_work(2.5)
+
+    producer.cancel()
+    consumer.cancel()
+
+    with contextlib.suppress(asyncio.CancelledError):
+        await producer
+
+    with contextlib.suppress(asyncio.CancelledError):
+        await consumer
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
